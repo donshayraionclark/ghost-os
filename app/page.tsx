978e1f86906a1463dc372c25e05a8b27d4f29230
donshayraionclark <donshayraionclark@icloud.com>
2026-03-25 17:38:04 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: 40, background: "black", color: "white", minHeight: "100vh" }}>
+      <h1>Ghost OS LIVE</h1>
+    </div>
+  );
+}
