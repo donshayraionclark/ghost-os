26b614ef1a97ac3417d5543ba88dde3bf5f63027
donshayraionclark <donshayraionclark@icloud.com>
2026-03-25 20:50:16 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
-export default function RootLayout({ children }) {
+export const metadata = {
+  title: "Ghost OS",
+  description: "Ghost OS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
